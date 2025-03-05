234c853c3f634d25dabf1f31d7b33764995988bd
Ivan Florentina <ciobanue016@gmail.com>
2025-03-05 20:17:27 +0200

    Add function for sales data

--- total_sum.py
+++ total_sum.py
@@ -1,1 +1,10 @@
 # this file calculate total sum of all products
+sales_data = [230, 200, 310, 290, 400, 150, 180]
+
+def total_sum(sales_data):
+    return sum(sales_data)
+
+total_sales = total_sum(sales_data)
+print(f"Total sales of the week: {sales_data}")
+
+
