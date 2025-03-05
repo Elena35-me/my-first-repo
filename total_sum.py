bdb1091505bc3991d13481aac2bde4e7484917fd
Ivan Florentina <ciobanue016@gmail.com>
2025-03-05 20:30:46 +0200

    Recalculate the total sales data

--- total_sum.py
+++ total_sum.py
@@ -5,6 +5,6 @@ def total_sum(sales_data):
     return sum(sales_data)
 
 total_sales = total_sum(sales_data)
-print(f"Total sales of the week: {sales_data}")
+print("Total sales of the week:", total_sales)
 
 
